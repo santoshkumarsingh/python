ab97fa1aafecf5690ed3188af38839dc1bdd81cf
santosh <santosh.ksingh0@gmail.com>
2012-06-14 21:49:55 +0530

    decorator example in python

---
+++ decorator.py
@@ -0,0 +1,22 @@
+'''Decorators allow you to inject or modify code in functions or
+classes.
+Suppose  you would like to do something at the entry and exit points of function
+such as pefrom some kind of security,tracing,locking etc..'''
+
+class decorator(object):
+    def __init__(self,fn):
+        self.fn=fn
+    def __call__(self,*args,**kwargs):
+        print "Start",self.fn.__name__
+        ret=self.fn(*args,**kwargs)
+        print "Stop",self.fn.__name__
+        return ret
+
+'''Test above decorator'''
+@decorator
+def fun_args(*args):
+    print len(args)
+    return sum(args)
+
+fun_args(1,2,3,4)
+
