bfca0a31af0eaa8302b8c86466219aa81ab5b874
santosh <santosh.ksingh0@gmail.com>
2012-06-16 23:04:28 +0530

    added trace function

--- PascalTriangle.py
+++ PascalTriangle.py
@@ -1,4 +1,25 @@
 '''This is simple implementation of pascal triangle in python'''
+level = 0
+def trace(f):
+    def g(*args):
+        global level
+        indent = '|   ' * level + '|-- '
+
+        argstr = ", ".join([repr(a) for a in args])
+        print indent + f.__name__ + argstr
+
+        level += 1
+        value = f(*args)
+        level -= 1
+
+        print indent + ' return ' + repr(value)
+        return value
+    return g
+
+
+
+
+@trace
 def pascal(n):
     if n == 1:
         return [ [1] ]
@@ -9,8 +30,12 @@ def pascal(n):
         return result
 
 
+
+
+
 if __name__=="__main__":
     h = int(raw_input("Please enter the height of the triangle: "))
+    print 
     for i in pascal(h):
         print " " * (h * 2), i
         h -= 1
